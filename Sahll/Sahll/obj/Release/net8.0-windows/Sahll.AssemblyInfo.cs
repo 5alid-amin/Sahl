ba3cc43b17034af2c2095a036ee1598bb37805a1
5aled <5aled.amin.sn@gmail.com>
2024-09-09 23:09:52 +0300

    initial setup to start working in sha2 allah

--- Sahll/Sahll/obj/Release/net8.0-windows/Sahll.AssemblyInfo.cs
+++ Sahll/Sahll/obj/Release/net8.0-windows/Sahll.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sahll")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca8dd6e089db048cba46dbf872a396f55e798b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sahll")]
